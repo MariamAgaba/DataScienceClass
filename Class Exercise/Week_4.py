3c8f29b7492f3b869245c4b47009645fa41ef34b
PROF. ADEDIRAN <mariam.mcadam@gmail.com>
2024-06-13 15:32:52 +0100

    week 4_loop

---
+++ Class Exercise/Week_4.py
@@ -0,0 +1,41 @@
+# #loops
+# #functions
+# #classes
+# print('hello')
+# for i in range(6):
+#     print(i)
+#     print(i+1)
+# for num in range(1,10):
+#     print(num)
+# for num in range(1,11):
+#     print(num)
+# for num in range(12):
+#     print(num)
+# for num in range(5,8,11):
+#     print(num)
+# for num in range(5,13, 2):
+#     print(num)
+# for num in range(13,2, -2):
+#     print(num)
+# for num in range(3,39, 3):
+#     print(num)
+# for num in range(6):
+#     print(num)
+#     print("Dami")
+#     print("please repeat me")
+# for num in range(1,13):
+#     print(num*3)
+LG_in_Kwara = ['Ilorin West', 'Ekiti', 'Oke-oro', 'Ifelodun', 'Barutee', 'asa']
+for LG in LG_in_Kwara:
+    print(LG[-1])
+for LG in range(len(LG_in_Kwara)):
+    print(LG_in_Kwara[LG][-1])
+
+
+
+
+
+
+
+   
+   
